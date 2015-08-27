d3c2583cd7b670c79fbe0a63eb2a0ab0feedfbc9
Thomas Theunen <thomas.theunen@gmail.com>
2015-08-27 19:50:37 +0200

    Added getters for ticket, project and user. Also added ability to update a ticket.

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # jQuery-JiraWrapper
+
+A basic jQuery wrapper for the Jira REST API. If you want to use this on your website, remind to allow CORS  on the JIRA server as this is not enabled by default. Information on how to do this is available here: https://jira.atlassian.com/browse/JRA-30371
+
+Currently only basic authentication is supported, but I will look at OAth later on.
+
+## HOW TO
+
+### Requirements
+
+To work this plugin requires jQuery 1.8 or higher.
+
+### Initialization
+
+You can initialize the plugin with the following code:
+
+'''javascript
+Jira.init({
+  baseUrl: 'https://jira.atlassian.com/',
+  username: 'username',
+  password: 'password'
+});
+'''
+
+### Fetching/updating/deleting an issue
+
+It is possible to fetch an issue and modify it.
+
+'''javascript
+  Jira.issue('TICKET-001');
+'''
+
+The following code deletes the issue if you have the permissions!
+
+'''javascript
+  Jira.issue('TICKET-001').delete();
+'''
+
+Or you can update a ticket using one of the following functions
+
+'''javascript
+  Jira.issue('TICKET-001').updateSummary('Summary Text');
+  Jira.issue('TICKET-001').updateName('Name of the ticket');
+'''
+
+Off course if you are planning to do multiple operations to the same ticket it is best to do that with less server requests like this:
+
+'''javascript
+  var issue = Jira.issue('TICKET-001'):
+
+  issue.updateSummary('Summary Text');
+  issue.updateName('Name of the ticket');
+'''
+
+Another option is to do multiple updates within the same request:
+
+'''javascript
+  var ticketUpdate = {
+    "update": {
+        "summary": [
+            {
+                "set": "Bug in business logic"
+            }
+        ],
+        "timetracking": [
+            {
+                "edit": {
+                    "originalEstimate": "1w 1d",
+                    "remainingEstimate": "4d"
+                }
+            }
+        ],
+        "labels": [
+            {
+                "add": "triaged"
+            },
+            {
+                "remove": "blocker"
+            }
+        ],
+        "components": [
+            {
+                "set": ""
+            }
+        ]
+    }
+  };
+
+  Jira.issue('TICKET-001').update(ticketUpdate);
+'''
--- jquery.jira.js
+++ jquery.jira.js
@@ -14,27 +14,102 @@ var Jira = {
     debug: false,
     oauth: false
   },
-  init: function(options) {
+  init: function (options) {
     this.options = $.extend({}, this.options, options);
 
     this.authenticate();
   },
-  authenticate: function() {
+  authenticate: function () {
     //Do nothing here yet.
   },
-  request: function(location) {
+  request: function (location, method, data) {
     var requestURL = this.options.baseUrl + location;
+
+    if(!method) {
+      method = "GET";
+    }
 
     return $.ajax({
       url: requestURL,
-      dataType: 'json',
+      dataType: 'JSON',
+      method: method,
+      contentType: 'application/json',
       async: false,
+      data: JSON.stringify(data),
       headers: {
-        'Authorization': "Basic " + btoa(Jira.options.username + ":" + Jira.options.password)
+        'Authorization': 'Basic ' + btoa(Jira.options.username + ':' + Jira.options.password)
       }
     }).responseJSON;
   },
-  issue: function(ticketNo) {
-    return this.request("rest/api/2/issue/" + ticketNo);
+  dashboard: function (dashboardId) {
+    return this.request('rest/api/2/dashboard' + ((dashboardId) ? '/' + dashboardId : ''));
+  },
+  issue: function (ticketNo) {
+    var jiraTicket = this.request('rest/api/2/issue/' + ticketNo);
+
+    return {
+      id: jiraTicket.id,
+      key: jiraTicket.key,
+      summary: jiraTicket.fields.summary,
+      description: jiraTicket.fields.description,
+      priority: jiraTicket.fields.priority,
+      status: jiraTicket.fields.status,
+      project: jiraTicket.fields.project,
+      created: jiraTicket.fields.created,
+      creator: jiraTicket.fields.creator,
+      reporter: jiraTicket.fields.reporter,
+      assignee: jiraTicket.fields.assignee,
+      progress: {
+        progress: jiraTicket.fields.progress.progress,
+        percent: jiraTicket.fields.progress.percent,
+        total: jiraTicket.fields.progress.total,
+        aggregated: jiraTicket.fields.aggregateprogress,
+        estimate: jiraTicket.fields.aggregatetimeestimate,
+        originalestimate: jiraTicket.fields.aggregatetimeoriginalestimate,
+        timespent: jiraTicket.fields.aggregatetimespent
+      },
+      links: jiraTicket.fields.issuelinks,
+      attachments: jiraTicket.fields.attachment,
+      comments: jiraTicket.fields.comment.comments,
+      worklog: jiraTicket.fields.worklog.worklogs,
+      jiraObject: jiraTicket,
+      update: function (updateObject) {
+          return Jira.request('rest/api/2/issue/' + this.key, 'PUT', updateObject);
+      },
+      updateSummary: function () {
+        // TODO
+      },
+      delete: function () {
+        return Jira.request('rest/api/2/issue/' + this.key, 'DELETE');
+      }
+    };
+  },
+  project: function (projectId) {
+    var jiraProject = this.request('rest/api/2/project/' + projectId);
+
+    return {
+      id: jiraProject.id,
+      key: jiraProject.key,
+      name: jiraProject.name,
+      description: jiraProject.description,
+      avatar: jiraProject.avatarUrls,
+      lead: jiraProject.lead,
+      roles: jiraProject.roles,
+      jiraObject: jiraProject
+    };
+  },
+  user: function (username) {
+    var jiraUser = this.request('rest/api/2/user?username=' + username);
+
+    return {
+      key: jiraUser.key,
+      name: jiraUser.name,
+      displayName: jiraUser.displayName,
+      emailAddress: jiraUser.emailAddress,
+      avatar: jiraUser.avatarUrls,
+      timezone: jiraUser.timeZone,
+      groups: jiraUser.groups,
+      jiraObject: jiraUser
+    };
   }
 };
